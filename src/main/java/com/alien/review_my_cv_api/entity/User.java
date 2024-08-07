01015ac8e4c23014f4b8dbdce40d4cf390cc95df
Muhammad Eid <meid95874@gmail.com>
2024-08-08 00:13:03 +0300

    Create Role and User repositories

--- src/main/java/com/alien/review_my_cv_api/entity/User.java
+++ src/main/java/com/alien/review_my_cv_api/entity/User.java
@@ -14,7 +14,6 @@ import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @Builder
---
+++ src/main/java/com/alien/review_my_cv_api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.alien.review_my_cv_api.repository;
+
+import com.alien.review_my_cv_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
