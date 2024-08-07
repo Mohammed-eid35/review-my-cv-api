4d9a8d79809ee1059988f8bd9168679223e60029
Muhammad Eid <meid95874@gmail.com>
2024-08-07 13:21:28 +0300

    Setting up the project dependances

---
+++ src/main/java/com/alien/review_my_cv_api/ReviewMyCvApiApplication.java
@@ -0,0 +1,13 @@
+package com.alien.review_my_cv_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewMyCvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewMyCvApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alien/review_my_cv_api/ReviewMyCvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alien.review_my_cv_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewMyCvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
